5cab1a3a43a6717247513837fe409c2aad20d6f7
Ngo Zong Han <zonghan01@gmail.com>
2025-02-22 15:02:09 +0800

    commit

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -3,41 +3,6 @@ import React, {useState, useEffect} from 'react'
 // dateTime Format "YYYY-MM-DDTHH:MM:SS"
 function Countdown(props){
 
-    const calculateTimeLeft = () => {
-        const difference = +new Date(props.targetDate) - +new Date();
-        let timeLeft = {};
-        
-        if (difference > 0) {
-          timeLeft = {
-            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-            minutes: Math.floor((difference / 1000 / 60) % 60),
-            seconds: Math.floor((difference / 1000) % 60)
-          };
-        }
-        return timeLeft;
-      };
-    
-      const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-    
-      // Update the timer every second
-      useEffect(() => {
-        const timer = setTimeout(() => {
-          setTimeLeft(calculateTimeLeft());
-        }, 1000);
-        
-        // Clean up the timer when the component unmounts
-        return () => clearTimeout(timer);
-      });
-    
-      // Create timer components for display
-      const timerComponents = Object.keys(timeLeft).map(interval => (
-        <span key={interval} className="mx-3 flex flex-col items-center text-lg font-semibold uppercase">
-            <p>{interval}</p>
-            <p>{timeLeft[interval]}</p>
-        </span>
-      ));
-
     return(
         <div className='pt-10 flex flex-col items-center w-auto'>
             <div className='py-5 flex flex-col items-center rounded-md bg-[#FADA7A]'>
