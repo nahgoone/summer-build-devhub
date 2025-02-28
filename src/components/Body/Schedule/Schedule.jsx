37375e72ba5bb11e6f2e42d859ee87b524d232c7
Ngo Zong Han <zonghan01@gmail.com>
2025-02-28 23:53:32 +0800

    centered schedule line

--- src/components/Body/Schedule/Schedule.jsx
+++ src/components/Body/Schedule/Schedule.jsx
@@ -4,7 +4,7 @@ function Schedule(){
         <div id='schedule' className='flex flex-col mt-20 items-center'>
             <h1 className='text-[#1E1E1E] uppercase justify-center text-center font-bold md:text-4xl sm:text-3xl text-2xl'>
                 Schedule</h1>
-            <div className='absolute z-[-1] bg-[#C6C6C6] w-1 h-[300px] mt-25 '></div>
+            <div className='absolute z-[-1] bg-[#C6C6C6] w-1 h-[300px] mt-25 mr-39 '></div>
             <div className='flex flex-col items-center mt-5 w-auto'>
                 <ScheduleCard color='#81BFDA' fontWeight='bold' header='WEEK 1'/>
                 <ScheduleCard color='#B1F0F7' fontWeight='600' header='26 May'/>
